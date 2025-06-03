97af845399faca5c143014e12937c1dee70a56b7
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2025-06-03 09:32:49 +0800

    initial uploads. added plans for what to impl.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,16 @@
 //! This library provides tools and utilities for creating and managing
 //! vector-based content management systems.
 
+//! Plans
+//! 
+//! 1. VectorStoreConnector trait for impl different vector stores
+//!     1. Qdrant impl
+//!     2. In-Memory impl
+//!     3. Provide methods: CRUD
+//! 2. Blog trait for impl blog create, read, update and delete methods
+//!     1. default impl 
+//!     2. Blog handles Category, and Category handles Post
+
 /// A placeholder function for the vertices library
 pub fn hello() -> String {
     "Hello from vertices library!".to_string()
